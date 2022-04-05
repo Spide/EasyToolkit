735fc474150b0581a28821986c3d1a1f865f7042
Tomáš Ciboch <t.ciboch@gmail.com>
2022-04-05 09:11:43 +0200

    pool from array

--- Poolers/GameObjectPool.cs
+++ Poolers/GameObjectPool.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 
 namespace Easy.Pooling
 {
-    public class GameObjectPool<T> : Pool<T> where T : MonoBehaviour
+    public class GameObjectPool<T> : Pool<T> where T : Component
     {
         public GameObjectPool(T objectToPool, Action<T> reset, int limit) : base(() => GameObject.Instantiate<T>(objectToPool), (T hidrate) => { reset?.Invoke(hidrate); hidrate.gameObject.SetActive(true); return hidrate; }, (T snooz) => { snooz.gameObject.SetActive(false); return snooz; }, (T destroy) => GameObject.Destroy(destroy.gameObject), limit)
         {
             snoozeMethod.Invoke(objectToPool);
+        }
+
+        public GameObjectPool(T[] objectToPool, Action<T> reset, int limit) : base(() => GameObject.Instantiate<T>(objectToPool[UnityEngine.Random.Range(0, objectToPool.Length)]), (T hidrate) => {  reset?.Invoke(hidrate); hidrate.gameObject.SetActive(true); return hidrate; }, (T snooz) => { snooz.gameObject.SetActive(false); return snooz; }, (T destroy) => GameObject.Destroy(destroy.gameObject), limit)
+        {
+           Array.ForEach(objectToPool, (i) => snoozeMethod.Invoke(i));
         }
     }
 }
